db32d048d2489ec81935737b89c64a08498e9376
dimitarrskv <dimitar.rskv@gmail.com>
2024-11-20 03:24:13 +0100

    chore: add docs folder for github pages

---
+++ docs/assets/metadata/martinruskov.en.ts
@@ -0,0 +1,80 @@
+export default {
+  lang: 'en',
+  title: 'Student of dental medicine',
+  location: 'in Medical university of Plovdiv',
+  introText: 'and I offer my services to anyone who wants to take care of their oral health and hygiene. The examination and consultation are free, and the prices of the other manipulations are symbolic. Come to my place or call for a consultation. I pay attention to each patient with professionalism and care. I am waiting for you!',
+  greeting: 'Hi, I\'m',
+  firstName: 'MARTIN',
+  lastName: 'RUSKOV',
+  aboutTitle: 'ABOUT',
+  aboutText: 'Martin Ruskov, a student of dental medicine at Plovdiv Medical University, offers professional services at symbolic prices, unreservedly striving for perfection in every procedure. His attention to detail and pursuit of perfectionism ensure high quality care for each patient. Martin always puts the comfort of his patients first, providing them with calm and safe treatment.',
+  
+  servicesTitle: 'SERVICES',
+  serviceList: [
+    {
+      name: 'Free examination and consultation',
+      price: 'Free'
+    },
+    {
+      name: 'Manufacturing of prostheses, crowns, bridges',
+      price: '5lv'
+    },
+    {
+      name: 'Tooth extraction',
+      price: '5lv'
+    },
+    {
+      name: 'Root treatment',
+      price: '5lv'
+    },
+    {
+      name: 'Caries treatment',
+      price: '5lv'
+    },
+    {
+      name: 'Cleaning tartar',
+      price: '5lv'
+    }
+  ],
+  servicesSubscript: 'All manipulations are performed under the strict supervision of an experienced specialist from the Faculty of Dental Medicine at the Medical University of Plovdiv, in order to guarantee the safety and quality of the procedures.',
+  contactsTitle: 'CONTACTS',
+  contactList: [
+    { label: '+359 892 481 190',
+      value: '+359 892 481 190',
+      type: 'phone'
+    },
+    { label: 'Plovdiv Center, bul. "Hristo Botev" 3',
+      value: 'Plovdiv Center, bul. "Hristo Botev" 3',
+      type: 'address'
+    },
+    { label: 'martiruskov0123@gmail.com',
+      value: 'martiruskov0123@gmail.com',
+      type: 'email'
+    },
+    { label: '@marti_ruskov',
+      value: 'https://www.instagram.com/marti_ruskov/?hl=en',
+      type: 'instagram'
+    },
+    { label: '+359 892 481 190',
+      value: 'viber://chat?number=359892481190',
+      type: 'viber'
+    },
+    { label: '+359 892 481 190',
+      value: 'https://wa.me/+359892481190',
+      type: 'whatsapp'
+    },
+    { label: 'martin.ruskov.7',
+      value: 'https://www.facebook.com/martin.ruskov.7',
+      type: 'facebook'
+    }
+  ],
+  contactsSuperscript: 'You can contact me on the phone number, email or private message on WhatsApp, Viber, Messenger and Instagram. I accept patients at the Faculty of Dental Medicine at the Medical University of Plovdiv',
+
+  googleMapsOptions: {
+    mapId: "7847a4f9644a6262",
+    center: { lat: 42.133748, lng: 24.733428 },
+    zoom: 17
+  },
+
+  googleMapsLink: "https://www.google.com/maps/place/%D0%A4%D0%B0%D0%BA%D1%83%D0%BB%D1%82%D0%B5%D1%82+%D0%BF%D0%BE+%D0%B4%D0%B5%D0%BD%D1%82%D0%B0%D0%BB%D0%BD%D0%B0+%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D0%B0+-+%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2/@42.1336607,24.730868,17z/data=!3m1!4b1!4m6!3m5!1s0x14acd0348a726cdb:0x4ff46e3dbfb4e402!8m2!3d42.1336568!4d24.7334429!16s%2Fg%2F1vc8lncr?entry=ttu&g_ep=EgoyMDI0MDkyOS4wIKXMDSoASAFQAw%3D%3D"
+}
